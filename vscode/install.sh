8bfad9285a63edcc27e01aa6c509827ccd6d13f3
Christian Gambardella <christian.gambardella@gmail.com>
2019-10-09 22:30:16 +0000

    adds vs code folder

---
+++ vscode/install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+set -e
+
+echo "Todo: Copy settings/keymaps etc"
+
+echo "Installing extensions..."
+code --install-extension BriteSnow.vscode-toggle-quotes
+code --install-extension closedcontour.very-smart-select
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension deerawan.vscode-dash
+code --install-extension eamodio.gitlens
+code --install-extension Equinusocio.vsc-material-theme
+code --install-extension eriklynd.json-tools
+code --install-extension esbenp.prettier-vscode
+code --install-extension felipecaputo.git-project-manager
+code --install-extension Gruntfuggly.todo-tree
+code --install-extension ionutvmi.path-autocomplete
+code --install-extension midnightsyntax.vscode-wrap-console-log
+code --install-extension mikestead.dotenv
+code --install-extension mitchdenny.ecdc
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension octref.vetur
+code --install-extension oderwat.indent-rainbow
+code --install-extension PKief.material-icon-theme
+code --install-extension redhat.vscode-yaml
+code --install-extension ritwickdey.LiveServer
+code --install-extension RobbOwen.synthwave-vscode
+code --install-extension robinbentley.sass-indented
+code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension sysoev.language-stylus
+code --install-extension wmaurer.change-case
+code --install-extension xyz.local-history
+code --install-extension yzhang.markdown-all-in-one
+echo "Done."
