ee2f68b194dbe6b19f2ff076c8c03b38450c296e
Christian Gambardella <christian.gambardella@gmail.com>
2021-04-30 16:41:39 +0200

    adds comment in nvim.sh

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -6,6 +6,6 @@ mkdir -p .config
 # Symlinks config
 ln -s $HOME/.dotfiles/nvim $HOME/.config/nvim
 
+# Installs vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
