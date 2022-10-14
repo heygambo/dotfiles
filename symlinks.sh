46b92717a62f5324918071edce60358c3b52da57
Christian Gambardella <hey@gambo.io>
2022-10-14 11:38:47 +0200

    adds symlinks.sh

---
+++ symlinks.sh
@@ -0,0 +1,14 @@
+# synlink zsh
+ln -s -f $HOME/.dotfiles/zsh/zlogin $HOME/.zlogin
+ln -s -f $HOME/.dotfiles/zsh/zlogout $HOME/.zlogout
+ln -s -f $HOME/.dotfiles/zsh/zpreztorc $HOME/.zpreztorc
+ln -s -f $HOME/.dotfiles/zsh/zprofile $HOME/.zprofile
+ln -s -f $HOME/.dotfiles/zsh/zshenv $HOME/.zshenv
+ln -s -f $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
+
+# symlink tmux
+ln -s -f $HOME/.dotfiles/submodules/tmux/.tmux.conf $HOME/.tmux.conf
+ln -s -f $HOME/.dotfiles/tmux/tmux.conf.local $HOME/.tmux.conf.local
+
+# synlink power terminal
+ln -s -f $HOME/.dotfiles/zsh/p10k.zsh $HOME/.p10k.zsh
