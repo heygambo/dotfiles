dbf0c308455b2e0164c82df4bf4d9eb1adda2cc4
Christian Gambardella <hey@gambo.io>
2022-10-21 23:57:54 +0200

    adds astro config

--- symlinks.sh
+++ symlinks.sh
@@ -1,4 +1,4 @@
-# synlink zsh
+# symlink zsh
 ln -s -f $HOME/.dotfiles/zsh/zlogin $HOME/.zlogin
 ln -s -f $HOME/.dotfiles/zsh/zlogout $HOME/.zlogout
 ln -s -f $HOME/.dotfiles/zsh/zpreztorc $HOME/.zpreztorc
@@ -10,5 +10,8 @@ ln -s -f $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
 ln -s -f $HOME/.dotfiles/submodules/tmux/.tmux.conf $HOME/.tmux.conf
 ln -s -f $HOME/.dotfiles/tmux/tmux.conf.local $HOME/.tmux.conf.local
 
-# synlink power terminal
+# symlink power terminal
 ln -s -f $HOME/.dotfiles/zsh/p10k.zsh $HOME/.p10k.zsh
+
+# Test if folder "~/.config/nvim/lua" exists
+ln -s -f $HOME/.dotfiles/nvim-astro $HOME/.config/nvim/lua/user
